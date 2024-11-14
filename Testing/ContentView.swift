5dc4ef97612768f17b7271b0229782a641a6879d
33Decibel <geeta.negi@33decibel.com>
2024-11-14 16:40:08 +0530

    First commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by 33 Decibel on 18/08/1946 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, !")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by 33 Decibel on 18/08/1946 Saka.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
